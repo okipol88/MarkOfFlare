28f1149e2b0b207567b39619a8ac2486377b2bf6
bs <bs@macrix.pl>
2020-11-11 00:08:31 +0100

    Dividing code into seperate classes for clarity

--- MarkOfFlare/Program.cs
+++ MarkOfFlare/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MvvmBlazor.Extensions;
+using MarkOfFlare.ViewModel;
 
 namespace MarkOfFlare
 {
@@ -18,6 +20,10 @@ namespace MarkOfFlare
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMvvm();
+
+            builder.Services.AddTransient<IFlareWizarClaimViewModel, FlareClaimViewModel>();
 
             await builder.Build().RunAsync();
         }
---
+++ MarkOfFlare/ViewModel/FlareClaimViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using MvvmBlazor.ViewModel;
+
+namespace MarkOfFlare.ViewModel
+{
+    public interface IFlareWizarClaimViewModel
+    {
+        public int Progress { get; set; }
+
+        public bool PreviousButtonDisabled { get; set; }
+
+        public bool NextButtonDisabled { get; set; }
+
+        public int CurrentStep { get; set; }
+
+        public EventCallback GoToPreviousStep { get; set; }
+
+        public EventCallback GoToNextStep { get; set; }
+    }
+
+    public class FlareClaimViewModel: ViewModelBase, IFlareWizarClaimViewModel
+    {
+        public FlareClaimViewModel()
+        {
+        }
+
+        private int progress;
+        private bool previousButtonDisabled;
+        private bool nextButtonDisabled;
+        private int currentStep = 1;
+
+        public int Progress
+        {
+            get => progress;
+            set => Set(ref progress, value);
+        }
+
+        public bool PreviousButtonDisabled
+        {
+            get => previousButtonDisabled;
+            set => Set(ref previousButtonDisabled, value);
+        }
+
+        public bool NextButtonDisabled
+        {
+            get => nextButtonDisabled;
+            set => Set(ref nextButtonDisabled, value);
+        }
+
+        public int CurrentStep
+        {
+            get => currentStep;
+            set => Set(ref currentStep, value);
+        }
+
+        public EventCallback GoToPreviousStep { get; set; }
+
+
+        public EventCallback GoToNextStep { get; set; }
+    }
+}
